328f4b1ec9dae8f5077632e84a4b068954117e9a
th <thalisson1240@gmail.com>
2026-05-05 15:24:14 -0300

    Conexao banco de dados quase la

--- src/testConexao/TesteConexao.java
+++ src/testConexao/TesteConexao.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package conexao;
+package testConexao;
 
 /**
  *
  * @author Professor
  */
-public class conexaoBD {
+public class TesteConexao {
     
 }
